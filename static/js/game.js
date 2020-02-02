68acc80f31e88e3a4db6e0430eee8ce2cfe92de2
Stefan Gherman <scgheman@gmail.com>
2020-02-02 02:56:00 +0200

    Add simple display for symbols.

--- static/js/game.js
+++ static/js/game.js
@@ -8,3 +8,29 @@ retryButton.addEventListener("click", function(event){
     sessionStorage.currentTurn = "X";
     window.location.reload(true);
 });
+
+const gameBoardSquare = document.querySelectorAll('.game-cell');
+
+const xSymbol = document.createElement("p");
+xSymbol.setAttribute("style", "font-size:24px;")
+xSymbol.textContent = "X"
+
+const oSymbol = document.createElement("p");
+oSymbol.setAttribute("style", "font-size:24px;")
+oSymbol.textContent = "O"
+
+
+for (let gameBoard of gameBoardSquare) {
+    gameBoard.addEventListener('click', function (event) {
+        let pressed = event.target;
+        console.log(pressed.getAttribute("class"), pressed);
+        if (sessionStorage.currentTurn === 'X'){
+            pressed.textContent = xSymbol.innerHTML;
+            sessionStorage.currentTurn ='O';
+        } else if(sessionStorage.currentTurn === 'O') {
+            pressed.textContent = oSymbol.innerHTML;
+            sessionStorage.currentTurn='X';
+        }
+
+    });
+}
