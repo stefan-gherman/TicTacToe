df0bd6c3098dc8fce98c0057dd5cc49c675fbf79
Stefan Gherman <scgheman@gmail.com>
2020-02-02 02:55:22 +0200

    Add reload functionality to the "Try Again" button.

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,10 @@
-alert('You should write code into "static/js/game.js" to make it work');
+//alert('You should write code into "static/js/game.js" to make it work');
+
+sessionStorage.setItem("currentTurn", "X")
+
+retryButton = document.getElementById("retry-button");
+
+retryButton.addEventListener("click", function(event){
+    sessionStorage.currentTurn = "X";
+    window.location.reload(true);
+});
